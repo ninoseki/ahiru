a7fae03be4fdc5703635fb1a611c3caaeea004bc
Manabu Niseki <manabu.niseki@gmail.com>
2019-03-12 05:54:23 +0900

    chore: make arguments more declarative

--- lib/ahiru/api.rb
+++ lib/ahiru/api.rb
@@ -8,7 +8,8 @@ module Ahiru
   class API
     DUCKDUCKGO_ENDPOINT = "https://duckduckgo.com/html/"
 
-    def search(params)
+    def search(q:, s: nil, dc: nil)
+      params = { q: q, s: s, dc: dc }.compact
       body = get_body(DUCKDUCKGO_ENDPOINT, params)
       return nil if body.nil?
 
